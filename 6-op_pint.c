338f1424fdaeb1286cbd64db53d302498f094ec5
wikki2000 <wisdomokposin@gmail.com>
2023-12-16 17:54:04 -0800

    Betty compliant

--- 6-op_pint.c
+++ 6-op_pint.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 
 /**
- * op_pint - prints the value at the top of the stack, followed by a new line.
- * @stack: 
+ * op_pint - Prints the value at the top of the stack, followed by a new line.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the Monty bytecode file
  */
 void op_pint(stack_t **stack, unsigned int line_number)
 {
-	line_number = 1;
-
 	if (*stack == NULL)
 	{
-		printf("L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
 	printf("%d\n", (*stack)->n);
 }
+
